b59b0edaf6e5fda2ecf02dc212bc35794d6475c0
Heming Hanevik <Heming.Hanevik@student.uib.no>
2022-11-07 12:49:26 +0100

    fixed issue creating database userdata.db

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 import flask_login
 from flask_login import login_required, login_user, UserMixin
+import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = secrets.token_urlsafe(16)
@@ -37,11 +38,6 @@ cssData = HtmlFormatter(nowrap=True).get_style_defs('.highlight')
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = "login"
-
-
-users = {'alice' : {'password' : 'password123', 'token' : 'tiktok'},
-         'bob' : {'password' : 'bananas'}
-         }
 
 # Class to store user info
 # UserMixin provides us with an `id` field and the necessary
@@ -84,58 +80,9 @@ class LoginForm(FlaskForm):
 # the User object for a given user id
 @login_manager.user_loader
 def user_loader(user_id):
-    if user_id not in User.query.id.data:
-        return
-
-    # For a real app, we would load the User from a database or something
-    user = User()
-    user.id = user_id
-    return user
+    return User.query.get(int(user_id))
 
 
-# This method is called to get a User object based on a request,
-# for example, if using an api key or authentication token rather
-# than getting the user name the standard way (from the session cookie)
-@login_manager.request_loader
-def request_loader(request):
-    # Even though this HTTP header is primarily used for *authentication*
-    # rather than *authorization*, it's still called "Authorization".
-    auth = request.headers.get('Authorization')
-
-    # If there is not Authorization header, do nothing, and the login
-    # manager will deal with it (i.e., by redirecting to a login page)
-    if not auth:
-        return
-
-    (auth_scheme, auth_params) = auth.split(maxsplit=1)
-    auth_scheme = auth_scheme.casefold()
-    if auth_scheme == 'basic':  # Basic auth has username:password in base64
-        (uid,passwd) = b64decode(auth_params.encode(errors='ignore')).decode(errors='ignore').split(':', maxsplit=1)
-        print(f'Basic auth: {uid}:{passwd}')
-        u = users.get(uid)
-        if u: # and check_password(u.password, passwd):
-            return user_loader(uid)
-    elif auth_scheme == 'bearer': # Bearer auth contains an access token;
-        # an 'access token' is a unique string that both identifies
-        # and authenticates a user, so no username is provided (unless
-        # you encode it in the token – see JWT (JSON Web Token), which
-        # encodes credentials and (possibly) authorization info)
-        print(f'Bearer auth: {auth_params}')
-        for uid in users:
-            if users[uid].get('token') == auth_params:
-                return user_loader(uid)
-    # For other authentication schemes, see
-    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Authentication
-
-    # If we failed to find a valid Authorized header or valid credentials, fail
-    # with "401 Unauthorized" and a list of valid authentication schemes
-    # (The presence of the Authorized header probably means we're talking to
-    # a program and not a user in a browser, so we should send a proper
-    # error message rather than redirect to the login page.)
-    # (If an authenticated user doesn't have authorization to view a page,
-    # Flask will send a "403 Forbidden" response, so think of
-    # "Unauthorized" as "Unauthenticated" and "Forbidden" as "Unauthorized")
-    abort(HTTPStatus.UNAUTHORIZED, www_authenticate = WWWAuthenticate('Basic realm=inf226, Bearer'))
 
 def pygmentize(text):
     if not hasattr(tls, 'formatter'):
@@ -157,7 +104,7 @@ def favicon_png():
 
 
 @app.route('/')
-@app.route('/index.html')
+@app.route('/index')
 @login_required
 def index_html():
     return send_from_directory(app.root_path,
@@ -174,7 +121,8 @@ def register():
         db.session.commit()
         return redirect(url_for('login'))
 
-    return render_template('./register.html', form=form)
+    else:
+        return render_template('./register.html', form=form)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -185,41 +133,17 @@ def login():
         if user:
             if bcrypt.check_password_hash(user.password, form.password.data):
                 login_user(user)
-                return redirect(url_for("/"))
-        
-    return render_template('login.html', form=form)
+                flask.flash('Logged in successfully.')
+                return redirect(url_for('index_html'))
+    else:    
+        return render_template('./login.html', form=form)
 
-
-    if form.is_submitted():
-        print(f'Received form: {"invalid" if not form.validate() else "valid"} {form.form_errors} {form.errors}')
-        print(request.form)
-    if form.validate_on_submit():
-        # TODO: we must check the username and password
-        username = form.username.data
-        password = form.password.data
-        u = users.get(username)
-        if u: # and check_password(u.password, password):
-            user = user_loader(username)
             
-            # automatically sets logged in session cookie
-            login_user(user)
-
-            flask.flash('Logged in successfully.')
-
-            next = flask.request.args.get('next')
-    
-            # is_safe_url should check if the url is safe for redirects.
-            # See http://flask.pocoo.org/snippets/62/ for an example.
-            if False and not is_safe_url(next):
-                return flask.abort(400)
-
-            return flask.redirect(next or flask.url_for('index'))
-    return render_template('./login.html', form=form)
-
 @app.get('/search')
 def search():
-    query = request.args.get('q') or request.form.get('q') or '*'
-    stmt = f"SELECT * FROM messages WHERE message GLOB '{query}'"
+    user = flask_login.current_user.username
+    query = request.args.get('q') or request.form.get('q')
+    stmt = f"SELECT * FROM messages WHERE message LIKE '{query}' AND (sender='{user}' OR receiver='{user}')"
     result = f"Query: {pygmentize(stmt)}\n"
     try:
         c = conn.execute(stmt)
@@ -232,14 +156,35 @@ def search():
     except Error as e:
         return (f'{result}ERROR: {e}', 500)
 
+@app.get('/show')
+def show():
+    user = flask_login.current_user.username
+    stmt = f"SELECT * FROM messages WHERE sender='{user}' OR receiver='{user}'"
+    result = f"Query: {pygmentize(stmt)}\n"
+    try:
+        c = conn.execute(stmt)
+        rows = c.fetchall()
+        result = result + 'Result:\n'
+        for row in rows:
+            result = f'{result}    {dumps(row)}\n'
+        c.close()
+        return result
+    except Error as e:
+        return (f'{result}ERROR: {e}', 500)
+
+
 @app.route('/send', methods=['POST','GET'])
 def send():
     try:
-        sender = request.args.get('sender') or request.form.get('sender')
+        time = datetime.datetime.now()
+        sender = flask_login.current_user.username
+        receiver = request.args.get('receiver') or request.form.get('receiver')
         message = request.args.get('message') or request.args.get('message')
-        if not sender or not message:
-            return f'ERROR: missing sender or message'
-        stmt = f"INSERT INTO messages (sender, message) values ('{sender}', '{message}');"
+        if not receiver or not message:
+            return f'ERROR: missing receiver or message'
+        if not User.query.filter_by(username=receiver).first():
+            return f'ERROR: user not found'
+        stmt = f"INSERT INTO messages (time, sender, receiver, message) values ('{time}', '{sender}', '{receiver}', '{message}');"
         result = f"Query: {pygmentize(stmt)}\n"
         conn.execute(stmt)
         return f'{result}ok'
@@ -269,7 +214,9 @@ try:
     c = conn.cursor()
     c.execute('''CREATE TABLE IF NOT EXISTS messages (
         id integer PRIMARY KEY, 
-        sender TEXT NOT NULL,
+        time DATETIME,
+        sender VARCHAR(20),
+        receiver VARCHAR(20),
         message TEXT NOT NULL);''')
     c.execute('''CREATE TABLE IF NOT EXISTS announcements (
         id integer PRIMARY KEY, 
